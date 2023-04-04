1bf3c84d79d0a0865465b059bf6e582972257f25
cruway <astralkaiser89@gmail.com>
2023-04-04 23:05:45 +0900

    spring security - filterChain

--- src/main/java/com/example/springsecurity/common/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/common/config/SecurityConfig.java
@@ -12,10 +12,27 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // login権限
         http.authorizeHttpRequests()
                 .anyRequest()
                 .authenticated();
-        http.formLogin();
+
+        http.formLogin()
+                //.loginPage("/loginPage") // ユーザ定義ログインページ
+                .defaultSuccessUrl("/") // ログイン成功移動ページ
+                .failureUrl("/login") // ログイン失敗移動ページ
+                .usernameParameter("userId") // IDパラメータ名設定
+                .passwordParameter("password") // パスワードパラメタ名設定
+                .loginProcessingUrl("/login_proc") // ログインForm Action Url
+                .successHandler((request, response, authentication) -> {
+                    System.out.println("authentication = " + authentication.getName());
+                    response.sendRedirect("/");
+                }) // ログイン成功後、ハンドラー
+                .failureHandler((request, response, exception) -> {
+                    System.out.println("exception = " + exception.getMessage());
+                    response.sendRedirect("/login");
+                }) // ログイン失敗後、ハンドラー
+                .permitAll();
         return http.build();
     }
 }
