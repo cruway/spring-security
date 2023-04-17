4809c12aa04786a7e301e7a337d24410f34f8726
cruway <astralkaiser89@gmail.com>
2023-04-17 14:18:20 +0900

    基本権限ページ構築

--- src/main/java/com/example/springsecurity/repository/ResourcesRepositoryImpl.java
+++ src/main/java/com/example/springsecurity/repository/ResourcesRepositoryImpl.java
@@ -1,22 +1,46 @@
 package com.example.springsecurity.repository;
 
 import com.example.springsecurity.domain.Resources;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+import static com.example.springsecurity.domain.QResources.resources;
+import static com.example.springsecurity.domain.QRole.role;
+
+@RequiredArgsConstructor
 public class ResourcesRepositoryImpl implements ResourcesRepositoryQuery {
+
+    private final JPAQueryFactory queryFactory;
+
     @Override
     public List<Resources> findAllResources() {
-        return null;
+        return queryFactory
+                .selectFrom(resources)
+                .join(resources.roleSet, role)
+                .where(resources.resourceType.eq("url"))
+                .orderBy(resources.orderNum.desc())
+                .fetch();
     }
 
     @Override
     public List<Resources> findAllMethodResources() {
-        return null;
+        return queryFactory
+                .selectFrom(resources)
+                .join(resources.roleSet, role)
+                .where(resources.resourceType.eq("method"))
+                .orderBy(resources.orderNum.desc())
+                .fetch();
     }
 
     @Override
     public List<Resources> findAllPointcutResources() {
-        return null;
+        return queryFactory
+                .selectFrom(resources)
+                .join(resources.roleSet, role)
+                .where(resources.resourceType.eq("pointcut"))
+                .orderBy(resources.orderNum.desc())
+                .fetch();
     }
 }
